4c81504f59405a4d9bb25e39f8fb91230c577f10
baeyc0510 <bjj5573@gmail.com>
2026-01-07 22:53:16 +0900

    feat: add PRAnimInstance

---
+++ Source/ProjectReboot/Animation/PRAnimInstance.cpp
@@ -0,0 +1,110 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PRAnimInstance.h"
+
+#include "KismetAnimationLibrary.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "ProjectReboot/Character/PlayerCharacter.h"
+
+void UPRAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	PlayerCharacter = Cast<APlayerCharacter>(GetOwningActor());
+	if (IsValid(PlayerCharacter))
+	{
+		CharacterMovement = PlayerCharacter->GetCharacterMovement();
+	}
+}
+
+void UPRAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (!IsValid(PlayerCharacter) || !IsValid(CharacterMovement))
+	{
+		return;
+	}
+	
+	UpdateVelocity();
+	UpdateAcceleration();
+	UpdateDirection();
+	UpdateAim();
+	UpdateLean();
+	UpdateFlags();
+	
+	//TODO: TurnInPlace
+}
+
+void UPRAnimInstance::UpdateVelocity()
+{
+	Velocity = CharacterMovement->Velocity;
+	Velocity2D = FVector(Velocity.X, Velocity.Y, 0.0f);
+	XYSpeed = Velocity.Size2D();
+	
+	FRotator ActorRotation = PlayerCharacter->GetActorRotation();
+	LocalVelocity2D = ActorRotation.UnrotateVector(Velocity2D);
+}
+
+void UPRAnimInstance::UpdateAcceleration()
+{
+	Acceleration = CharacterMovement->GetCurrentAcceleration();
+	
+	FRotator ActorRotation = PlayerCharacter->GetActorRotation();
+	FVector AccelerationXY = FVector(Acceleration.X, Acceleration.Y, 0.0f); 
+	LocalAcceleration2D = ActorRotation.UnrotateVector(AccelerationXY);
+}
+void UPRAnimInstance::UpdateDirection()
+{
+	FRotator ActorRotation = PlayerCharacter->GetActorRotation();
+	Direction =  UKismetAnimationLibrary::CalculateDirection(Velocity2D,ActorRotation);
+	if (Direction >= -70.f && Direction <= 70.f)
+	{
+		CardinalDirection = ECardinalDirection::Forward;
+	}
+	else if (Direction > 70.f && Direction <= 110.f)
+	{
+		CardinalDirection = ECardinalDirection::Right;
+	}
+	else if (Direction >= -110.f && Direction < -70.f)
+	{
+		CardinalDirection = ECardinalDirection::Left;
+	}
+	else
+	{
+		CardinalDirection = ECardinalDirection::Backward;
+	}
+	
+	F_OrientationAngle = Direction;
+	R_OrientationAngle = Direction - 90.f;
+	B_OrientationAngle = Direction - 180.f;
+	L_OrientationAngle = Direction + 90.f;
+}
+
+void UPRAnimInstance::UpdateFlags()
+{
+	bHasAcceleration = !LocalAcceleration2D.IsNearlyZero();
+	bShouldMove = XYSpeed > MoveSpeedThreshold && bHasAcceleration;
+	bIsFalling = CharacterMovement->IsFalling();
+	
+	// TODO: Character 상태 적용
+	bIsCrouching = false;
+	bIsSprint = false;
+	LandState = ELandState::Normal;
+}
+
+void UPRAnimInstance::UpdateAim()
+{
+	FRotator AimRotation = PlayerCharacter->GetBaseAimRotation();
+	FRotator ActorRotation = PlayerCharacter->GetActorRotation();
+	AimPitch = UKismetMathLibrary::NormalizedDeltaRotator(AimRotation, ActorRotation).Pitch;
+}
+
+void UPRAnimInstance::UpdateLean()
+{
+	float TurnDirection = PlayerCharacter->GetTurnDirection();
+	float DeltaSeconds = GetWorld()->GetDeltaSeconds();
+	LeanDirection = FMath::FInterpTo(LeanDirection, TurnDirection, DeltaSeconds, 10.f);
+}
---
+++ Source/ProjectReboot/Animation/PRAnimInstance.h
@@ -0,0 +1,110 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PRAnimationTypes.h"
+#include "Animation/AnimInstance.h"
+#include "PRAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+class APlayerCharacter;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREBOOT_API UPRAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+private:
+	void UpdateVelocity();
+	void UpdateAcceleration();
+	void UpdateDirection();
+	void UpdateFlags();
+	void UpdateAim();
+	void UpdateLean();
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<APlayerCharacter> PlayerCharacter;
+	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UCharacterMovementComponent> CharacterMovement;
+	
+	/*~ Movements ~*/
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float MoveSpeedThreshold = 3.0f;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector Velocity;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector Velocity2D;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector Acceleration;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector LocalVelocity2D;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector LocalAcceleration2D;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float XYSpeed;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float CameraX;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float Direction;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	ECardinalDirection CardinalDirection;
+	
+	// 전방 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float F_OrientationAngle;
+	
+	// 우측 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float R_OrientationAngle;
+	
+	// 후방 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float B_OrientationAngle;
+	
+	// 좌측 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float L_OrientationAngle;
+	
+	// 기울어짐 방향
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float LeanDirection;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bHasAcceleration;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bShouldMove;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsCrouching;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsSprint;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	ELandState LandState;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement|Aiming")
+	float AimPitch;
+	
+};

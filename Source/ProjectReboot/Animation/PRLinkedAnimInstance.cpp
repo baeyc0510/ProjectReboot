cf54706be7276e669a9cadc3df6f13e5f496248b
baeyc0510 <bjj5573@gmail.com>
2026-01-07 22:53:16 +0900

    feat: 플레이어 기본 로코모션
    feat: add PRAnimInstance
    
    feat: add TurnInPlace functions to animinstance
    
    feat: complete base player locomotion

---
+++ Source/ProjectReboot/Animation/PRLinkedAnimInstance.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PRLinkedAnimInstance.h"
+
+#include "PRAnimInstance.h"
+
+void UPRLinkedAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	MainAnimInstance = Cast<UPRAnimInstance>(Blueprint_GetMainAnimInstance());
+}
+
+void UPRLinkedAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (!IsValid(MainAnimInstance))
+	{
+		return;
+	}
+	
+	bIsSprint = MainAnimInstance->bIsSprint;
+	F_OrientationAngle = MainAnimInstance->F_OrientationAngle;
+	R_OrientationAngle = MainAnimInstance->R_OrientationAngle;
+	B_OrientationAngle = MainAnimInstance->B_OrientationAngle;
+	L_OrientationAngle = MainAnimInstance->L_OrientationAngle;
+	LeanDirection = MainAnimInstance->LeanDirection;
+	CardinalDirection = MainAnimInstance->CardinalDirection;
+}
---
+++ Source/ProjectReboot/Animation/PRLinkedAnimInstance.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PRAnimationTypes.h"
+#include "Animation/AnimInstance.h"
+#include "PRLinkedAnimInstance.generated.h"
+
+class UPRAnimInstance;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREBOOT_API UPRLinkedAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UPRAnimInstance> MainAnimInstance;
+	
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsSprint;
+	
+	// 전방 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float F_OrientationAngle;
+	
+	// 우측 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float R_OrientationAngle;
+	
+	// 후방 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float B_OrientationAngle;
+	
+	// 좌측 기준 방향 각도
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float L_OrientationAngle;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float LeanDirection;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	ECardinalDirection CardinalDirection;
+};

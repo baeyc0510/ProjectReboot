7b88959436a2578058458e63dc94ca579ac70814
baeyc0510 <baeyc0510@gmail.com>
2026-01-09 15:01:13 +0900

    wip: testing equipment changing system

--- Source/ProjectReboot/ProjectReboot.Build.cs
+++ Source/ProjectReboot/ProjectReboot.Build.cs
@@ -8,9 +8,19 @@ public class ProjectReboot : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraphRuntime" });
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"GameplayAbilities",
+			"GameplayTags",
+			"AnimGraphRuntime",
+			"RogueliteCore",
+			"RogueliteGAS",
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });

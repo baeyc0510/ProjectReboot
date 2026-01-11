8c9ecce832ec81589c1f26424fc7932187a62b7a
baeyc0510 <baeyc0510@gmail.com>
2026-01-11 20:35:29 +0900

    refactor: RogueliteSystem 개선 (#2)
    ### 변경 사항
    
      **1. SlotSystem 제거**
      - RogueliteSubsystem의 Slot 관련 API 제거
      - FRogueliteSlotArray, FRogueliteSlotSaveArray 구조체 제거
      - 슬롯 관리는 EquipmentManager에서 독립적으로 처리
    
      **2. 태그 필터링된 이벤트 시스템 추가**
      - FRogueliteActionFilteredSignature (Native Delegate) 추가
    - BindActionAcquiredByTags / BindActionRemovedByTags /
    BindStackChangedByTags
      - 특정 태그를 가진 Action 변경 시에만 콜백 수신 가능
      - FDelegateHandle 반환으로 개별 언바인딩 지원
    
      ### 사용 예시
      ```cpp
      FGameplayTagContainer FilterTags;
      FilterTags.AddTag(FGameplayTag::RequestGameplayTag("Equipment"));
    
      FDelegateHandle Handle = Subsystem->BindActionAcquiredByTags(
          FilterTags,
    FRogueliteActionFilteredSignature::FDelegate::CreateUObject(this,
    &ThisClass::OnEquipmentAcquired)
      );

---
+++ Source/ProjectReboot/Equipment/PREquipmentManagerComponent.cpp
@@ -0,0 +1,132 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PREquipmentManagerComponent.h"
+
+#include "RogueliteSubsystem.h"
+#include "ProjectReboot/Roguelite/PRRogueliteEquipActionData.h"
+
+
+// Sets default values for this component's properties
+UPREquipmentManagerComponent::UPREquipmentManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPREquipmentManagerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	BindToRogueliteSubsystem();
+}
+
+void UPREquipmentManagerComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	
+	UnbindFromRogueliteSubsystem();
+}
+
+void UPREquipmentManagerComponent::HandleActionAcquired(URogueliteActionData* Action, int32 OldStacks, int32 NewStacks)
+{
+	UPRRogueliteEquipActionData* EquipActionData = Cast<UPRRogueliteEquipActionData>(Action);
+	if (!IsValid(EquipActionData))
+	{
+		return;
+	}
+	
+	
+}
+
+void UPREquipmentManagerComponent::HandleActionRemoved(URogueliteActionData* Action, int32 OldStacks, int32 NewStacks)
+{
+	UPRRogueliteEquipActionData* EquipActionData = Cast<UPRRogueliteEquipActionData>(Action);
+	if (!IsValid(EquipActionData))
+	{
+		return;
+	}
+	
+	
+}
+
+void UPREquipmentManagerComponent::HandleStackChanged(URogueliteActionData* Action, int32 OldStacks, int32 NewStacks)
+{
+	UPRRogueliteEquipActionData* EquipActionData = Cast<UPRRogueliteEquipActionData>(Action);
+	if (!IsValid(EquipActionData))
+	{
+		return;
+	}
+	
+	
+}
+
+void UPREquipmentManagerComponent::HandleRunEnded(bool bCompleted)
+{
+}
+
+bool UPREquipmentManagerComponent::EquipActionToSlot(UPRRogueliteEquipActionData* Action, FGameplayTag SlotTag)
+{
+	return false;
+}
+
+void UPREquipmentManagerComponent::UnequipActionFromSlot(UPRRogueliteEquipActionData* Action,
+	FGameplayTag SlotTag)
+{
+}
+
+TArray<UPRRogueliteEquipActionData*> UPREquipmentManagerComponent::GetSlotContents(FGameplayTag SlotTag) const
+{
+	return TArray<UPRRogueliteEquipActionData*>();
+}
+
+int32 UPREquipmentManagerComponent::GetSlotCount(FGameplayTag SlotTag) const
+{
+	return 0;
+}
+
+bool UPREquipmentManagerComponent::IsSlotFull(FGameplayTag SlotTag, int32 MaxCount) const
+{
+	return false;
+}
+
+
+void UPREquipmentManagerComponent::BindToRogueliteSubsystem()
+{
+	URogueliteSubsystem* RogueliteSubsystem = URogueliteSubsystem::Get(this);
+	if (!IsValid(RogueliteSubsystem))
+	{
+		return;
+	}
+
+	// TODO: Tag기반으로 특정 이벤트만 수신
+	RogueliteSubsystem->OnActionAcquired.AddDynamic(this, &ThisClass::HandleActionAcquired);
+	RogueliteSubsystem->OnActionRemoved.AddDynamic(this, &ThisClass::HandleActionRemoved);
+	RogueliteSubsystem->OnStackChanged.AddDynamic(this, &ThisClass::HandleStackChanged);
+	RogueliteSubsystem->OnRunEnded.AddDynamic(this, &ThisClass::HandleRunEnded);
+	
+	bIsBoundToSubsystem = true;
+}
+
+void UPREquipmentManagerComponent::UnbindFromRogueliteSubsystem()
+{
+	if (!bIsBoundToSubsystem)
+	{
+		return;
+	}
+	
+	URogueliteSubsystem* RogueliteSubsystem = URogueliteSubsystem::Get(this);
+	if (!IsValid(RogueliteSubsystem))
+	{
+		return;
+	}
+	
+	RogueliteSubsystem->OnActionAcquired.RemoveDynamic(this, &ThisClass::HandleActionAcquired);
+	RogueliteSubsystem->OnActionRemoved.RemoveDynamic(this, &ThisClass::HandleActionRemoved);
+	RogueliteSubsystem->OnStackChanged.RemoveDynamic(this, &ThisClass::HandleStackChanged);
+	RogueliteSubsystem->OnRunEnded.RemoveDynamic(this, &ThisClass::HandleRunEnded);
+}
---
+++ Source/ProjectReboot/Equipment/PREquipmentTypes.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PREquipmentTypes.generated.h"
+
+
+// 장비 부착 소켓 정보
+USTRUCT(BlueprintType)
+struct FEquipmentAttachmentInfo
+{
+	GENERATED_BODY()
+
+	// 부착할 소켓 이름
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attachment")
+	FName SocketName;
+
+	// 위치 오프셋
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attachment")
+	FVector LocationOffset = FVector::ZeroVector;
+
+	// 회전 오프셋
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attachment")
+	FRotator RotationOffset = FRotator::ZeroRotator;
+
+	// 스케일
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attachment")
+	FVector Scale = FVector::OneVector;
+};
+
+// 메시 스폰 정보
+USTRUCT(BlueprintType)
+struct FEquipmentMeshSpawnInfo
+{
+	GENERATED_BODY()
+
+	// 스태틱 메시
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TObjectPtr<UStaticMesh> StaticMesh;
+
+	// 스켈레탈 메시
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TObjectPtr<USkeletalMesh> SkeletalMesh;
+
+	// 부착 정보
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	FEquipmentAttachmentInfo AttachmentInfo;
+
+	// 머티리얼 오버라이드 (인덱스별)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TMap<int32, TObjectPtr<UMaterialInterface>> MaterialOverrides;
+};
+
+USTRUCT(BlueprintType)
+struct FEquipmentVisualSettings
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	FEquipmentMeshSpawnInfo MeshSpawnInfo;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	TArray<TSubclassOf<UAnimInstance>> AnimLayersToLink;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	TObjectPtr<UAnimMontage> EquipMontage;
+	// TODO: 장착 SFX 혹은 VFX?
+};

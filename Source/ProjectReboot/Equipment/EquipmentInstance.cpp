4b31907876b97bcaf1cd87677811896042cb1090
baeyc0510 <baeyc0510@gmail.com>
2026-01-12 17:06:48 +0900

    feat: 무기 부품 교체 시스템 구현 (#8)
    ### 개요
    무기 부품 교체 시스템 구현. EquipmentInstance가 ActionData 기반으로 비주얼을 관리하며, 태그 조건에 따라
    동적으로 외형이 변경됨.
    
    ### 주요 변경
    
    #### EquipmentInstance
    - ActionData 기반 비주얼 스폰/제거 (AddVisual, RemoveVisual)
    - FRogueliteTagCountContainer로 장비 태그 관리
    - Rule 기반 비주얼 선택 (장비 태그가 룰 태그 조건 충족 시 메시 변경)
    - RefreshVisuals()로 태그 변경 시 비주얼 자동 갱신
    
    #### EquipmentManager (WIP)
    - UPREquipActionData로 타입 변경
    - 태그 기반 델리게이트 바인딩
    - FEquipmentSlotItem, FEquipmentSlots 구조체 추가
    - 기존 슬롯 메서드 제거
    - Equip/Unequip 로직 미구현
    
    ### 사용 예시
    BP\Test\BP_TestVisualActor를 통해 예시 확인 가능
    ```
    무기 장착 -> Instance 생성 -> Initialize -> AddVisuals
    ```
    
    ### TODO
    - EquipmentManager Equip/Unequip 구현
    - EquipmentManager 활성화시 기존에 이미 획득한 액션들 처리
    
    ## 참고
    기존 PR #6 에서 누락되었던 커밋을 복구하여 다시 적용함
    
    Closes #5

---
+++ Source/ProjectReboot/Equipment/EquipmentInstance.cpp
@@ -0,0 +1,262 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EquipmentInstance.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "ProjectReboot/Roguelite/PREquipActionData.h"
+
+void UEquipmentInstance::Initialize(USceneComponent* InAttachTarget, UPREquipActionData* InPrimaryActionData)
+{
+    AttachTarget = InAttachTarget;
+    PrimaryActionData = InPrimaryActionData;
+
+    if (PrimaryActionData)
+    {
+        AddVisual(PrimaryActionData);
+    }
+}
+
+void UEquipmentInstance::Uninitialize()
+{
+    DestroyAllVisuals();
+    AttachTarget.Reset();
+    PrimaryActionData = nullptr;
+}
+
+void UEquipmentInstance::AddVisual(UPREquipActionData* InActionData)
+{
+    if (!InActionData || !AttachTarget.IsValid())
+    {
+        return;
+    }
+
+    if (SpawnedVisuals.Contains(InActionData))
+    {
+        return;
+    }
+
+    // 태그 추가
+    GrantedTags.AppendTags(InActionData->TagsToGrant);
+
+    // 스폰 정보 선택
+    FEquipmentMeshSpawnInfo SpawnInfo = SelectSpawnInfo(InActionData->EquipmentVisualSettings);
+
+    if (!SpawnInfo.IsValid())
+    {
+        // 메시 없어도 태그는 등록됨
+        SpawnedVisuals.Add(InActionData, FSpawnedVisualEntry());
+        return;
+    }
+
+    // 컴포넌트 생성
+    USceneComponent* NewComponent = CreateMeshComponent(SpawnInfo);
+
+    FSpawnedVisualEntry Entry;
+    Entry.SpawnedComponent = NewComponent;
+    Entry.UsedSpawnInfo = SpawnInfo;
+
+    SpawnedVisuals.Add(InActionData, Entry);
+}
+
+void UEquipmentInstance::RemoveVisual(UPREquipActionData* InActionData)
+{
+    if (!InActionData)
+    {
+        return;
+    }
+
+    FSpawnedVisualEntry* Entry = SpawnedVisuals.Find(InActionData);
+    if (!Entry)
+    {
+        return;
+    }
+
+    // 컴포넌트 제거
+    if (Entry->SpawnedComponent)
+    {
+        Entry->SpawnedComponent->DestroyComponent();
+    }
+
+    SpawnedVisuals.Remove(InActionData);
+
+    // 태그 제거
+    GrantedTags.RemoveTags(InActionData->TagsToGrant);
+}
+
+void UEquipmentInstance::RefreshVisuals()
+{
+    if (!AttachTarget.IsValid())
+    {
+        return;
+    }
+
+    for (auto& Pair : SpawnedVisuals)
+    {
+        UPREquipActionData* Data = Pair.Key;
+        FSpawnedVisualEntry& Entry = Pair.Value;
+
+        FEquipmentMeshSpawnInfo NewSpawnInfo = SelectSpawnInfo(Data->EquipmentVisualSettings);
+
+        // 변경 여부 체크 (메시 포인터로 간단 비교)
+        bool bNeedsRefresh = false;
+
+        if (NewSpawnInfo.MeshType != Entry.UsedSpawnInfo.MeshType)
+        {
+            bNeedsRefresh = true;
+        }
+        else if (NewSpawnInfo.MeshType == EEquipmentMeshType::StaticMesh &&
+                 NewSpawnInfo.StaticMesh != Entry.UsedSpawnInfo.StaticMesh)
+        {
+            bNeedsRefresh = true;
+        }
+        else if (NewSpawnInfo.MeshType == EEquipmentMeshType::SkeletalMesh &&
+                 NewSpawnInfo.SkeletalMesh != Entry.UsedSpawnInfo.SkeletalMesh)
+        {
+            bNeedsRefresh = true;
+        }
+
+        if (bNeedsRefresh)
+        {
+            // 기존 컴포넌트 제거
+            if (Entry.SpawnedComponent)
+            {
+                Entry.SpawnedComponent->DestroyComponent();
+                Entry.SpawnedComponent = nullptr;
+            }
+
+            // 새 컴포넌트 생성
+            if (NewSpawnInfo.IsValid())
+            {
+                Entry.SpawnedComponent = CreateMeshComponent(NewSpawnInfo);
+            }
+
+            Entry.UsedSpawnInfo = NewSpawnInfo;
+        }
+    }
+}
+
+void UEquipmentInstance::DestroyAllVisuals()
+{
+    for (auto& Pair : SpawnedVisuals)
+    {
+        if (Pair.Value.SpawnedComponent)
+        {
+            Pair.Value.SpawnedComponent->DestroyComponent();
+        }
+    }
+
+    SpawnedVisuals.Empty();
+    GrantedTags.Reset();
+}
+
+FEquipmentMeshSpawnInfo UEquipmentInstance::SelectSpawnInfo(const FEquipmentVisualSettings& VisualSettings) const
+{
+    if (!VisualSettings.bUseRuleBasedMeshSpawnInfo)
+    {
+        return VisualSettings.DefaultMeshSpawnInfo;
+    }
+
+    // Priority 내림차순 정렬을 위한 복사
+    TArray<FRuleBasedMeshVisualSetting> SortedRules = VisualSettings.RuleBasedMeshSpawnSettings;
+    SortedRules.Sort([](const FRuleBasedMeshVisualSetting& A, const FRuleBasedMeshVisualSetting& B)
+    {
+        return A.Priority > B.Priority;
+    });
+
+    // 조건 충족하는 첫 번째 룰 반환
+    for (const FRuleBasedMeshVisualSetting& Rule : SortedRules)
+    {
+        if (GrantedTags.HasAll(Rule.RequiredTags))
+        {
+            return Rule.MeshSpawnInfo;
+        }
+    }
+
+    return VisualSettings.DefaultMeshSpawnInfo;
+}
+
+USceneComponent* UEquipmentInstance::CreateMeshComponent(const FEquipmentMeshSpawnInfo& SpawnInfo)
+{
+    AActor* Owner = AttachTarget.IsValid() ? AttachTarget->GetOwner() : nullptr;
+    if (!Owner)
+    {
+        return nullptr;
+    }
+
+    USceneComponent* NewComponent = nullptr;
+
+    switch (SpawnInfo.MeshType)
+    {
+    case EEquipmentMeshType::StaticMesh:
+        if (SpawnInfo.StaticMesh)
+        {
+            UStaticMeshComponent* StaticMeshComp = NewObject<UStaticMeshComponent>(Owner);
+            StaticMeshComp->SetStaticMesh(SpawnInfo.StaticMesh);
+            NewComponent = StaticMeshComp;
+        }
+        break;
+
+    case EEquipmentMeshType::SkeletalMesh:
+        if (SpawnInfo.SkeletalMesh)
+        {
+            USkeletalMeshComponent* SkelMeshComp = NewObject<USkeletalMeshComponent>(Owner);
+            SkelMeshComp->SetSkeletalMesh(SpawnInfo.SkeletalMesh);
+            NewComponent = SkelMeshComp;
+        }
+        break;
+
+    default:
+        break;
+    }
+
+    if (NewComponent)
+    {
+        ApplyAttachment(NewComponent, SpawnInfo.AttachmentInfo);
+        NewComponent->RegisterComponent();
+    }
+
+    return NewComponent;
+}
+
+void UEquipmentInstance::ApplyAttachment(USceneComponent* Component, const FEquipmentAttachmentInfo& AttachInfo)
+{
+    if (!Component || !AttachTarget.IsValid())
+    {
+        return;
+    }
+
+    Component->AttachToComponent(
+        AttachTarget.Get(),
+        FAttachmentTransformRules::SnapToTargetIncludingScale,
+        AttachInfo.SocketName
+    );
+
+    Component->SetRelativeLocation(AttachInfo.LocationOffset);
+    Component->SetRelativeRotation(AttachInfo.RotationOffset);
+    Component->SetRelativeScale3D(AttachInfo.Scale);
+}
+
+FGameplayTag UEquipmentInstance::GetSlotTag() const
+{
+    return PrimaryActionData ? PrimaryActionData->EquipmentSlot : FGameplayTag();
+}
+
+USceneComponent* UEquipmentInstance::GetPrimaryComponent() const
+{
+    if (PrimaryActionData)
+    {
+        const FSpawnedVisualEntry* Entry = SpawnedVisuals.Find(PrimaryActionData);
+        if (Entry)
+        {
+            return Entry->SpawnedComponent;
+        }
+    }
+
+    return nullptr;
+}
+
+bool UEquipmentInstance::HasVisual(UPREquipActionData* InActionData) const
+{
+    return SpawnedVisuals.Contains(InActionData);
+}
--- Source/ProjectReboot/Equipment/PREquipmentTypes.h
+++ Source/ProjectReboot/Equipment/PREquipmentTypes.h
@@ -1,9 +1,16 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "NativeGameplayTags.h"
 
 #include "PREquipmentTypes.generated.h"
 
+/*~ Equipment Tags ~*/
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_EQUIPMENT);
+
+/*~ 전방 선언 ~*/
+class UStaticMesh;
+class USkeletalMesh;
 
 // 장비 부착 소켓 정보
 USTRUCT(BlueprintType)
@@ -28,27 +35,60 @@ struct FEquipmentAttachmentInfo
 	FVector Scale = FVector::OneVector;
 };
 
+UENUM(BlueprintType)
+enum class EEquipmentMeshType : uint8
+{
+	SkeletalMesh,
+	StaticMesh,
+};
+
 // 메시 스폰 정보
 USTRUCT(BlueprintType)
 struct FEquipmentMeshSpawnInfo
 {
 	GENERATED_BODY()
 
-	// 스태틱 메시
+	// 메시 타입
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	EEquipmentMeshType MeshType = EEquipmentMeshType::SkeletalMesh;
+	
+	// 스태틱 메시
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh", meta = (EditCondition = "MeshType == EEquipmentMeshType::StaticMesh"))
 	TObjectPtr<UStaticMesh> StaticMesh;
 
 	// 스켈레탈 메시
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh", meta = (EditCondition = "MeshType == EEquipmentMeshType::SkeletalMesh"))
 	TObjectPtr<USkeletalMesh> SkeletalMesh;
 
 	// 부착 정보
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
 	FEquipmentAttachmentInfo AttachmentInfo;
 
-	// 머티리얼 오버라이드 (인덱스별)
+	// TODO: 머티리얼 오버라이드 (인덱스별)
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	// TMap<int32, TObjectPtr<UMaterialInterface>> MaterialOverrides;
+	
+public:
+	bool IsValid() const
+	{
+		return StaticMesh != nullptr || SkeletalMesh != nullptr;
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FRuleBasedMeshVisualSetting
+{
+	GENERATED_BODY()
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
-	TMap<int32, TObjectPtr<UMaterialInterface>> MaterialOverrides;
+	int32 Priority;
+	
+	// EquipmentInstance의 태그가 RequiredTags를 모두 충족할 때 이 세팅을 사용
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	FGameplayTagContainer RequiredTags;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	FEquipmentMeshSpawnInfo MeshSpawnInfo;
 };
 
 USTRUCT(BlueprintType)
@@ -57,12 +97,18 @@ struct FEquipmentVisualSettings
 	GENERATED_BODY()
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
-	FEquipmentMeshSpawnInfo MeshSpawnInfo;
+	FEquipmentMeshSpawnInfo DefaultMeshSpawnInfo;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	bool bUseRuleBasedMeshSpawnInfo;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh", meta = (EditCondition = "bUseRuleBasedMeshSpawnInfo"))
+	TArray<FRuleBasedMeshVisualSetting> RuleBasedMeshSpawnSettings;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
 	TArray<TSubclassOf<UAnimInstance>> AnimLayersToLink;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
 	TObjectPtr<UAnimMontage> EquipMontage;
-	// TODO: 장착 SFX 혹은 VFX?
+	// TODO: 장착 VFX?
 };

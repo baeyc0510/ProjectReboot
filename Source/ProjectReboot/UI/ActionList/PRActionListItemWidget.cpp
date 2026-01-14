c6c3d5762b55dd9c5c29545568e8cfeb0c6736df
baeyc0510 <baeyc0510@gmail.com>
2026-01-14 17:38:07 +0900

    wip: 액션 목록 위젯 구현중

---
+++ Source/ProjectReboot/UI/ActionList/PRActionListItemWidget.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PRActionListItemWidget.h"
+
+#include "RogueliteActionData.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UPRActionListItemWidget::InitWidget(URogueliteActionData* InActionData)
+{
+	if (!TB_ActionName || !TB_Description || !Img_Icon )
+	{
+		return;
+	}
+	
+	if (!InActionData)
+	{
+		TB_ActionName->SetText(FText());
+		TB_Description->SetText(FText());
+		Img_Icon->SetBrushFromTexture(nullptr);
+		
+		return;
+	}
+	
+	FText DisplayNameText = InActionData->GetFormattedTextWithValues(InActionData->DisplayName);
+	FText DescriptionText = InActionData->GetFormattedTextWithValues(InActionData->Description);
+	
+	TB_ActionName->SetText(DisplayNameText);
+	TB_Description->SetText(DescriptionText);
+	Img_Icon->SetBrushFromTexture(InActionData->Icon);
+}
---
+++ Source/ProjectReboot/UI/ActionList/PRActionListItemWidget.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PRActionListItemWidget.generated.h"
+
+class URogueliteActionData;
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREBOOT_API UPRActionListItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Roguelite")
+	void InitWidget(URogueliteActionData* InActionData);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<UTextBlock> TB_ActionName;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<UTextBlock> TB_Description;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<UImage> Img_Icon;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<URogueliteActionData> ActionData;
+};
